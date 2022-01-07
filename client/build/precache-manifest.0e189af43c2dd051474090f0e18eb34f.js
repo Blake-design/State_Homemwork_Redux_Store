27002f55b185f99d5605d6ef313dd8a0e425b9d1
Blake McCarty <blake_mccarty@yahoo.com>
2022-01-07 13:14:56 -0600

    updated app.js

---
+++ client/build/precache-manifest.0e189af43c2dd051474090f0e18eb34f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca8a4f618259a21abcd0a63306b8451d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "804146a605e878ea2946",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "87a4e0ff025a1a732f84",
+    "url": "/static/js/2.ecfb3f6e.chunk.js"
+  },
+  {
+    "revision": "f333c948d07dc08e5747a9b83a79e23a",
+    "url": "/static/js/2.ecfb3f6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "804146a605e878ea2946",
+    "url": "/static/js/main.cc42e59e.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
